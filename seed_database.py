deb0fa9b474b527bc0b0a2610ab0a8b4236e84ed
Tora Sirkka <tora@torasirkka.fi>
2021-06-03 11:07:23 -0700

    Refactored seed_database to match the datamodel refactor.

--- seed_database.py
+++ seed_database.py
@@ -1,25 +1,9 @@
-"""Script to seed database"""
-
-import os
-import json
-from random import choice, randint
-from datetime import datetime
-
-import crud
-import model
-import server
-
-os.system('dropdb podcasts')
-os.system('createdb podcasts')
-
-model.connect_to_db(server.app)
-model.db.create_all()
-
 """Script to seed database."""
 
 import os
 import json
 from datetime import datetime
+import uuid
 
 import crud
 import model
@@ -31,20 +15,48 @@ os.system("createdb podcasts")
 model.connect_to_db(server.app)
 model.db.create_all()
 
+def example_data_using_crud():
+    """Create test data using the CRUD functions.
+
+    Create five podcasts and three episodes per podcast."""
+    podcasts = []
+    episodes = []
+    for i in range(5):
+        pod = crud.add_podcast(
+            f"Podcast title {i}",
+            f"Test podcast description{i}",
+            img_url="www.test_img_url{i}.com",
+            )
+        podcasts.append(pod)
+
+        for i in range(3):
+            episode = pod.add_episode(
+                f"Episode0{i}",
+                f'Interesting content * {i}'
+            )
+            episodes.append(episode)
+
+    model.db.session.add_all(podcasts + episodes)
+    model.db.session.commit()
+
+example_data_using_crud()
+
 
 def example_data():
-    """Create some sample data."""
-    import uuid
+    """Create some test data by instantiating class objects directly."""
     podcast_id = str(uuid.uuid4())
-    test_pod = Podcast(podcast_id=podcast_id,
-                        description='Talkshow about racism',
-                        title= 'Yo, Is This Racist?',
-                        img_url='www.test_img_url.com')
+    test_pod = Podcast(
+        podcast_id=podcast_id,
+        description="Talkshow about racism",
+        title="Yo, Is This Racist?",
+        img_url="www.test_img_url.com",
+    )
 
-    test_episode = Episode(episode_id=str(uuid.uuid4()),
-                        podcast_id = podcast_id,
-                        episode_title = 'Episode2')
+    test_episode = test_pod.add_episode(
+        'Episode2','interesting content')
 
     db.session.add(test_pod)
     db.session.add(test_episode)
     db.session.commit()
+
+    example_data
