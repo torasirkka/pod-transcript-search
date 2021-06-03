1145bba587120cd1c1fb5be64dd6db513a0fbce3
Tora Sirkka <tora@torasirkka.fi>
2021-06-02 22:10:24 -0700

    Refactored my datamodel to more object oriented design.
    Implemented custom init methods in my Podcast and Episode classes. Made the CRUD method add_episode() an instance method on podcast objects.

--- crud.py
+++ crud.py
@@ -1,56 +1,31 @@
 "CRUD operations"
-from typing import Dict, List
+from typing import Dict, Any, Optional
 from datetime import datetime
 from model import db, Podcast, Episode, connect_to_db
-import uuid
 
-def add_new_podcast(title:str, description:str, img_url:str):
-    """Add a new podcast to the podcast table."""
 
-    podcast_id = str(uuid.uuid4())
-    podcast = Podcast(podcast_id=podcast_id,
-                        description=description,
-                        title=title,
-                        img_url=img_url)
+def add_podcast(title: str, description: str, img_url: str) -> Podcast:
+    """Add a new podcast to the podcasts table."""
 
+    podcast = Podcast(description, title, img_url)
     db.session.add(podcast)
-    db.session.commit()
 
     return podcast
 
-def add_new_episode(podcast_id: str, 
-                    title: str, 
-                    description: str, 
-                    release_date: datetime = '',
-                    transcript: str = {},
-                    status: str = '',
-                    ):
-    """Add a new episode to the episode table."""
 
-    episode_id = str(uuid.uuid4())
-    episode = Episode(episode_id = episode_id,
-                        podcast_id = podcast_id,
-                        episode_title = title,
-                        description = description,
-                        release_date = release_date,
-                        transcript = transcript,
-                        status = status)
-
-    db.session.add(episode)
-    db.session.commit()
-
-    return episode
-
-if __name__=='__main__':
+if __name__ == "__main__":
     from server import app
-    connect_to_db(app)
 
-    # test_pod = add_new_podcast(description='Talkshow about racism',
-    #                     title= 'Yo, Is This Racist?',
-    #                     img_url='www.test_img_url.com')
-    
-    # podcast_id = test_pod.podcast_id
-    
-    # test_episode = Episode(episode_id=str(uuid.uuid4()),
-    #                     podcast_id = podcast_id,
-    #                     episode_title = 'Episode2')
+    connect_to_db(app)
+    db.create_all()
+
+    test_pod = add_podcast(
+        description="Talkshow about racism",
+        title="Yo, Is This Racist?",
+        img_url="www.test_img_url.com",
+    )
+
+    test_episode = test_pod.add_episode(
+        'Episode2','interesting content')
+
+    db.session.commit()
--- model.py
+++ model.py
@@ -1,56 +1,105 @@
 """Models for podcast transcript search app."""
 
+from typing import Optional, Dict, Any
+from datetime import datetime
+import uuid
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import JSON
 
 db = SQLAlchemy()
 
 
-def connect_to_db(flask_app, db_uri='postgresql:///podcasts', echo=True):
-    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-    flask_app.config['SQLALCHEMY_ECHO'] = echo
-    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def connect_to_db(flask_app, db_uri="postgresql:///podcasts", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.app = flask_app
     db.init_app(flask_app)
 
-    print('Connected to the db!')
+    print("Connected to the db!")
 
 
 class Podcast(db.Model):
     """A podcast."""
 
-    __tablename__ = 'podcasts'
+    __tablename__ = "podcasts"
 
     podcast_id = db.Column(db.String(36), primary_key=True, nullable=True)
     title = db.Column(db.String(100))
     img_url = db.Column(db.String(200))
     description = db.Column(db.Text())
 
-    episodes = db.relationship('Episode', backref='podcast')
+    episodes = db.relationship("Episode", backref="podcast")
 
     def __repr__(self):
-        return f'<Podcast podcast_id={self.podcast_id} title={self.title}>'
+        return f"<Podcast podcast_id={self.podcast_id} title={self.title}>"
 
+    def __init__(self, title: str, description: str, img_url: str):
+        """Custom init method that generates and attaches a UUID primary key to the object being instantiated."""
+        podcast_id = str(uuid.uuid4())
+        podcast = super().__init__(
+            podcast_id=podcast_id, title=title, description=description, img_url=img_url
+        )
+        return podcast
+
+    def add_episode(self,
+        title: str,
+        description:str,
+        release_date:datetime=datetime.today(),
+        transcript: Optional[Dict[Any,Any]]={},
+        status: str=""):
+        """Method to add a new episode to the episodes table"""
+        
+        episode = Episode(self.podcast_id, title, description, release_date, transcript, status)
+        db.session.add(episode)
+        return episode
 
 class Episode(db.Model):
     """A podcast episode."""
 
-    __tablename__ = 'episodes'
+    __tablename__ = "episodes"
 
     episode_id = db.Column(db.String(36), primary_key=True)
-    podcast_id = db.Column(db.String(36), db.ForeignKey('podcasts.podcast_id'), nullable=False)
+    podcast_id = db.Column(
+        db.String(36), db.ForeignKey("podcasts.podcast_id"), nullable=False
+    )
     episode_title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text())
     release_date = db.Column(db.DateTime())
     transcript = db.Column(db.JSON)
     status = db.Column(db.String(10))
 
-    def __repr__(self):
-        return f'<Episode episode_id={self.episode_id} episode_title={self.episode_title}>'   
+    def __init__(
+        self,
+        podcast_id: str,
+        title: str,
+        description: str,
+        release_date: datetime,
+        transcript: Optional[Dict[Any, Any]],
+        status: str,
+    ):
+        """Custom init method that generates and attaches a UUID primary key to the object being instantiated."""
+        episode_id = str(uuid.uuid4())
+        episode = super().__init__(
+            episode_id=episode_id,
+            podcast_id=podcast_id,
+            episode_title=title,
+            description=description,
+            release_date=release_date,
+            transcript=transcript,
+            status=status,
+        )
+        return episode
 
-    
-if __name__ == '__main__':
+    def __repr__(self):
+        return (
+            f"<Episode episode_id={self.episode_id} episode_title={self.episode_title}>"
+        )
+
+
+if __name__ == "__main__":
     from server import app
 
     # Call connect_to_db(app, echo=False) if your program output gets
