e1ef8a37eb62387cc773f2341eab491ef37bab3d
Tora Sirkka <tora@torasirkka.fi>
2021-06-04 11:37:23 -0700

    Removed custom init methods.
    They affected readability in a negative way.

--- model.py
+++ model.py
@@ -9,26 +9,14 @@ from sqlalchemy.dialects.postgresql import JSON
 
 db = SQLAlchemy()
 
-
-def connect_to_db(flask_app, db_uri="postgresql:///podcasts", echo=True):
-    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
-    flask_app.config["SQLALCHEMY_ECHO"] = echo
-    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    db.app = flask_app
-    db.init_app(flask_app)
-
-    print("Connected to the db!")
-
-
 class Podcast(db.Model):
     """A podcast."""
 
     __tablename__ = "podcasts"
 
-    podcast_id = db.Column(db.String(36), primary_key=True, nullable=True)
-    title = db.Column(db.String(100))
-    img_url = db.Column(db.String(200))
+    podcast_id = db.Column(db.String(36), primary_key=True, nullable=False)
+    title = db.Column(db.String(500))
+    img_url = db.Column(db.String(500))
     description = db.Column(db.Text())
 
     episodes = db.relationship("Episode", backref="podcast")
@@ -36,25 +24,6 @@ class Podcast(db.Model):
     def __repr__(self):
         return f"<Podcast podcast_id={self.podcast_id} title={self.title}>"
 
-    def __init__(self, title: str, description: str, img_url: str):
-        """Custom init method that generates and attaches a UUID primary key to the object being instantiated."""
-        podcast_id = str(uuid.uuid4())
-        podcast = super().__init__(
-            podcast_id=podcast_id, title=title, description=description, img_url=img_url
-        )
-        return podcast
-
-    def add_episode(self,
-        title: str,
-        description:str,
-        release_date:datetime=datetime.today(),
-        transcript: Optional[Dict[Any,Any]]={},
-        status: str=""):
-        """Method to add a new episode to the episodes table"""
-        
-        episode = Episode(self.podcast_id, title, description, release_date, transcript, status)
-        db.session.add(episode)
-        return episode
 
 class Episode(db.Model):
     """A podcast episode."""
@@ -65,38 +34,27 @@ class Episode(db.Model):
     podcast_id = db.Column(
         db.String(36), db.ForeignKey("podcasts.podcast_id"), nullable=False
     )
-    episode_title = db.Column(db.String(100), nullable=False)
+    episode_title = db.Column(db.String(100))
     description = db.Column(db.Text())
     release_date = db.Column(db.DateTime())
     transcript = db.Column(db.JSON)
     status = db.Column(db.String(10))
 
-    def __init__(
-        self,
-        podcast_id: str,
-        title: str,
-        description: str,
-        release_date: datetime,
-        transcript: Optional[Dict[Any, Any]],
-        status: str,
-    ):
-        """Custom init method that generates and attaches a UUID primary key to the object being instantiated."""
-        episode_id = str(uuid.uuid4())
-        episode = super().__init__(
-            episode_id=episode_id,
-            podcast_id=podcast_id,
-            episode_title=title,
-            description=description,
-            release_date=release_date,
-            transcript=transcript,
-            status=status,
-        )
-        return episode
-
     def __repr__(self):
         return (
             f"<Episode episode_id={self.episode_id} episode_title={self.episode_title}>"
         )
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///podcasts", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
 
 
 if __name__ == "__main__":
