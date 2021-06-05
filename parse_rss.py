1d1a967809b75a8b2f0078c263555c0ef0efc55d
Tora Sirkka <tora@torasirkka.fi>
2021-06-05 06:32:20 -0700

    Added return variables to parse_episode_data()

--- parse_rss.py
+++ parse_rss.py
@@ -5,7 +5,6 @@ from typing import Tuple, Dict, Optional, Any
 import podcastparser
 import urllib.request
 
-file = open("RSS_feeds.txt")
 
 def get_rss_data(feed_url: str) -> Optional[Dict]:
     """Retrieve podcast data from feedurl."""
@@ -23,7 +22,10 @@ def parse_podcasts_data(rss_data: Dict[Any,Any]) -> Tuple[str]:
 def parse_episode_data(rss_episode_data: Dict[Any,Any]) -> Tuple[str]:
     """Parse out data needed to instantiate an episode object."""
     title = rss_episode_data['title']
-    description = rss_episode_data['description']
+    description_html = rss_episode_data['description']
     release_date = rss_episode_data['published']
+    mp3_url = rss_episode_data['enclosures'][2]['url']
+    guid = rss_episode_data['guid']
 
-    return (title, description, release_date)
+    return (title, description, release_date, mp3_url, guid)
+
