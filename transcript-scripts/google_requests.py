f312d5de2926049fe271934898b4092fea6fd07a
Tora Sirkka <tora@torasirkka.fi>
2021-06-17 14:09:36 -0700

    Added functions to list name of transcript files in bucket, and outline of functions to download and preprocess audio prior to initiating transcription.

---
+++ transcript-scripts/google_requests.py
@@ -0,0 +1,42 @@
+import os
+from typing import List
+from google.cloud import storage
+
+BUCKET_NAME = "audiofiles-storage"
+PREFIX = "transcripts"
+
+
+def file_is_in_bucket(
+    client: storage.Client, file_name: str, bucket_name: str = BUCKET_NAME
+) -> bool:
+    """Checks if a file with the name file_name is found in the bucket."""
+
+    names = _get_blob_names(client, bucket_name=bucket_name)
+    return file_name in names
+
+
+def _get_blob_names(client: storage.Client, bucket_name: str) -> List[str]:
+    """Get list of all filenames in the bucket."""
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = client.list_blobs(bucket_name, prefix=PREFIX)
+    # Each blob has an attribute 'name', which is the one we are after.
+    return [blob.name for blob in blobs]
+
+
+if __name__ == "__main__":
+    client = storage.Client()
+
+    # --------------------------------------------
+    # Tests for checking if transcription exists in bucket cache
+    test1 = _get_blob_names(client, "audiofiles-storage")
+    test2 = file_is_in_bucket(
+        client,
+        file_name="YoIsThisRacist_sample.flac",
+        bucket_name="audiofiles-storage",
+    )
+    test3 = file_is_in_bucket(
+        client,
+        file_name="transcripts/Yo_is_this_racist_sample.json",
+        bucket_name="audiofiles-storage",
+    )
--- transcript-scripts/plumbing.py
+++ transcript-scripts/plumbing.py
@@ -1,40 +1,74 @@
+# Input: episode. Output: transcript
+# getting example episode to work with:
+import sys
+
+sys.path.append(
+    "/Users/torasirkka/Documents/Hackbright2021/MyProject/podsearch/backend"
+)
+import model
+import server
+from urllib.parse import urlparse
 import os
 from typing import List
 from google.cloud import storage
+import google_requests
 
-BUCKET_NAME = "audiofiles-storage"
-PREFIX = "transcripts"
-
-
-def get_blob_names(client: storage.Client, bucket_name: str = BUCKET_NAME) -> List[str]:
-    """Get list of all filenames in the bucket."""
-
-    # Note: Client.list_blobs requires at least package version 1.17.0.
-    blobs = client.list_blobs(bucket_name, prefix=PREFIX)
-    # Each blob has an attribute 'name', which is the one we are after.
-    return [blob.name for blob in blobs]
+model.connect_to_db(server.app)
+STATUS = [None, "in progress", "done", "error"]
 
 
-def file_is_in_bucket(client: storage.Client, file_name: str, bucket_name: str) -> bool:
-    """Checks if a file with the name file_name is found in the bucket."""
+def initiate_episode_transcription() -> model.Episode:
+    """"""
+    episodes = episodes_without_transcript()
+    chosen_ep = episodes[0]
+    # updating but not yet committing status
+    chosen_ep.status = STATUS[1]
+    # checking if transcript exists in google bucket
+    fname = chosen_ep.fname
+    if google_requests.file_is_in_bucket(client=client, file_name=fname):
+        # TODO add fn that downloads, adds and commits transcript to db
+        pass
+    else:
+        transcribe_episode(chosen_ep)
 
-    names = get_blob_names(client, bucket_name=bucket_name)
-    return file_name in names
+    return chosen_ep
 
+
+def transcribe_episode(ep: model.Episode):
+    """Preprocess sound. If successful preprocess: transcribe episode. Set and commit status of episode."""
+    #     audio_flac = audio_download_and_processing(ep.mp3_url)
+
+    pass
+
+
+def episodes_without_transcript() -> List[model.Episode]:
+    """List episodes that do not have a transcription."""
+
+    return model.Episode.query.filter_by(status=None).all()
+
+
+def audio_download_and_processing(url: str):
+    """"""
+    pass
+
+
+def get_file_ext(url: str) -> str:
+    """Parse out the file extension from a url, indicating what the file type is of the linked data."""
+    file_type = os.path.basename(urlparse(url).path)
+    _, file_ext = os.path.splitext(file_type)
+    return file_ext
+
+
+url = "https://chrt.fm/track/FE12B2/traffic.omny.fm/d/clips/89050f29-3cfb-4513-a5d2-ac79004bd7ba/55c64838-70c7-4576-b4e4-ac800012ec27/dfee1f45-6630-41c9-a518-ad42004f34de/audio.mp3?utm_source=Podcast&in_playlist=05855b96-adce-4eaa-9d54-ac8300634c3"
+
+x = urlparse(url)
+y = os.path.basename(x.path)
+print(os.path.splitext(y))
+print(y)
+# subprocess.run()
 
 if __name__ == "__main__":
     client = storage.Client()
-
-    # --------------------------------------------
-    # Tests for checking if transcription exists in bucket cache
-    test1 = get_blob_names(client, "audiofiles-storage")
-    test2 = file_is_in_bucket(
-        client,
-        file_name="YoIsThisRacist_sample.flac",
-        bucket_name="audiofiles-storage",
-    )
-    test3 = file_is_in_bucket(
-        client,
-        file_name="transcripts/Yo_is_this_racist_sample.json",
-        bucket_name="audiofiles-storage",
-    )
+    # eps = episodes_without_transcript()
+    # initiate_episode_transcription()
+    print(audio_download_and_processing(url))
