c9195f8565b538cde7f5aa8f448fe21122c5ffc3
Tora Sirkka <tora@torasirkka.fi>
2021-06-18 17:29:17 -0700

    speach-to-text API call functions.

--- transcript-scripts/google_requests.py
+++ transcript-scripts/google_requests.py
@@ -1,9 +1,10 @@
 import os
 from typing import List
 from google.cloud import storage
+import requests
 
 BUCKET_NAME = "audiofiles-storage"
-AUDIO_FILE_PATH = BUCKET_NAME + "/audio-flac"
+AUDIO_FILE_PATH = "audiofiles-storage/audio-flac/"
 PREFIX = "transcripts"
 
 
@@ -29,16 +30,54 @@ from google.cloud import storage
 
 
 def upload_blob(
-    source_file_name: str, client: storage.Client(), bucket_name=BUCKET_NAME
+    source_file_name: str,
+    bucket_name: str,
+    target_file_path: str,
+    client: storage.Client(),
 ):
     """Uploads a file to the bucket."""
 
     bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(AUDIO_FILE_PATH + source_file_name)
+    blob = bucket.blob(target_file_path)
     # TODO fix timeout on upload!! Can I download directly to bucket? How to integrate with the rest of the transcription pipeline?
     blob.upload_from_filename(source_file_name)
 
-    print("File {} uploaded to {}.".format(source_file_name, destination_blob_name))
+    print("File {} uploaded to {}.".format(source_file_name, target_file_path))
+
+
+def transcribe_blob(
+    gc_audio_path: str, gc_results_path: str, client: storage.Client()
+) -> str:
+
+    """Initiate transcription of audiofile at gc_audio_path. Saver results
+    to gc_results_path."""
+
+    gcloud_config = {
+        "config": {
+            "language_code": "en-US",
+            "encoding": "FLAC",
+            "audio_channel_count": 1,
+            "enable_automatic_punctuation": True,
+            "enable_word_time_offsets": True,
+        },
+        "audio": {"uri": gc_audio_path},
+        "outputConfig": gc_results_path,
+    }
+
+    resp = requests.post(
+        "https://speech.googleapis.com/v1/speech:longrunningrecognize",
+        headers={
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        data=gcloud_config,
+    )
+    resp.json()  # {'name': 'gcloud operation name'}
+    return resp.json()
+
+
+def poll_transcription(gc_op_name: str):
+
+    return requests.get("https://speech.googleapis.com/v1/operations/" + gc_op_name)
 
 
 if __name__ == "__main__":
@@ -58,7 +97,7 @@ if __name__ == "__main__":
         bucket_name="audiofiles-storage",
     )
 
-    test4 = upload_blob(
-        "/Users/torasirkka/Documents/Hackbright2021/MyProject/podsearch/transcript-scripts/YoIsThisRacistCuriousGeorgeYOURdfee1f45663041c9a518ad42004f34de.flac",
-        client,
-    )
+    # test4 = upload_blob(
+    #     "/Users/torasirkka/Documents/Hackbright2021/MyProject/podsearch/transcript-scripts/YoIsThisRacistCuriousGeorgeYOURdfee1f45663041c9a518ad42004f34de.flac",
+    #     client,
+    # )
