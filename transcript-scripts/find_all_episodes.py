44f8e47c57345bd9fed3f7624e07c38d8ae39f85
Tora Sirkka <tora@torasirkka.fi>
2021-06-22 16:55:08 -0700

    RRefactor to reflect change from cache name (fname) being an episode attribute to being a function that takes an Episode object as input.

--- transcript-scripts/find_all_episodes.py
+++ transcript-scripts/find_all_episodes.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 sys.path.append(
     "/Users/torasirkka/Documents/Hackbright2021/MyProject/podsearch/backend"
@@ -18,7 +19,31 @@ def main():
 
     print("#!/bin/bash")
     for ep in episodes:
-        print(f"python3 transcribe_one.py '{ep.mp3_url}' '{ep.fname}'")
+        print(f"python3 transcribe_one.py '{ep.mp3_url}' '{fname(ep)}'")
+
+
+def fname(ep: model.Episode) -> str:
+    """Hash fn that generates a unique fname for a podcast episode.
+
+    This is the filename for the transcriptions cache stored used as back-up.
+    It consists of the first 15 chars of the podcast and episode titles + the
+    32 last episode uuid chars.
+    """
+
+    # TODO: check out https://docs.python.org/3/library/functools.html#functools.cached_property
+
+    # WARNING: LEGAL_FILENAME_CHARS is used in hash fn to generate unique ids. DO NOT ALTER! May cause
+    # transcription of files already transcribed!
+    LEGAL_FILENAME_CHARS = r"[a-zA-Z0-9]"
+
+    def only_legal_chars(s: str) -> str:
+        return "".join([char for char in s if re.match(LEGAL_FILENAME_CHARS, char)])
+
+    pod_title = only_legal_chars(ep.podcast.title)
+    ep_title = only_legal_chars(ep.episode_title)
+    ep_guid = only_legal_chars(ep.guid)
+
+    return pod_title[:17] + ep_title[:17] + ep_guid[-32:]
 
 
 if __name__ == "__main__":
